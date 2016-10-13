e0b49f14dd47c7350c7c0357954b89e45b81949b
mandos <anddy926@163.com>
2016-10-13 13:34:28 +0800

    JSPath 热修复线上Bug Demo

---
+++ JSPathUpdate/JSPathUpdate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSPathUpdate
+//
+//  Created by 金玉衡 on 16/10/13.
+//  Copyright © 2016年 mit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JSPathUpdate/JSPathUpdate/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JSPathUpdate
+//
+//  Created by 金玉衡 on 16/10/13.
+//  Copyright © 2016年 mit. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
